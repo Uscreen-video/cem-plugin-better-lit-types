6cda4ada489340a3f25710ceac32c02265998271
Bitaru <i@bitaru.com>
2023-01-31 08:05:40 +0100

    fix(): rename slotted component

--- src/render/index.ts
+++ src/render/index.ts
@@ -25,7 +25,7 @@ const getAttrsAndSlots = (props: any, types: any, { wrapSlots }: Settings): [Rec
     if (types[key]?.table?.category === 'slots') {
      slots += !wrapSlots || key === 'slot'
         ? props[key]
-        : `<slotted slot="${key}">${props[key]}</slotted>`
+        : `<tmp-component slot="${key}">${props[key]}</tmp-component>`
     } else {
       attributes[key] = props[key]
     }
